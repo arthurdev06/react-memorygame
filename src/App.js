31a10398bc78f86f2ebfb1cbf08d6750ce7364e7
Arthur Rocha <116519847+arthurdev06@users.noreply.github.com>
2022-11-29 14:25:33 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { useState } from "react";
+import Cards from "./Components/Cards";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Brawl Stars</h1>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
